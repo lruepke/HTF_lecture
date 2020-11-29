a733c0f88d512077bc6aa0a8b54b1456a0933257
lruepke <lars.ruepke@gmail.com>
2020-11-29 01:15:29 +0100

    sphinx setup

--- source/conf.py
+++ source/conf.py
@@ -28,11 +28,6 @@ author = 'Lars Ruepke'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages',
-    'nbsphinx',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'IPython.sphinxext.ipython_directive'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
