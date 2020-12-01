6320b75e9212cb0a7f37c1a2065b18eb02d56b90
zguo <zguo@geomar.de>
2020-12-01 13:50:20 +0100

    update

--- source/lectures/L04/cases/Jupp_Schultz/run.sh
+++ source/lectures/L04/cases/Jupp_Schultz/run.sh
@@ -9,10 +9,11 @@ application=`getApplication`
 # 1. clean old results
 ./clean.sh
 # 2. generate mesh using gmsh
-gmsh gmsh/mesh.geo -3 -o gmsh/mesh.msh -format msh22
-# 3. convert gmsh format to OpenFOAM format
-gmshToFoam gmsh/mesh.msh 
-# 4. set empty patch for 2D case
-changeDictionary
+# gmsh gmsh/mesh.geo -3 -o gmsh/mesh.msh -format msh22
+# # 3. convert gmsh format to OpenFOAM format
+# gmshToFoam gmsh/mesh.msh 
+# # 4. set empty patch for 2D case
+# changeDictionary
+blockMesh
 # 5. run 
 runApplication $application
