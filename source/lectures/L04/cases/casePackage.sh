7366a802998470cf9abb13ebd087cf640471663e
zguo <zguo@geomar.de>
2020-12-01 16:56:37 +0100

    update

--- source/lectures/L04/cases/casePackage.sh
+++ source/lectures/L04/cases/casePackage.sh
@@ -1,6 +1,11 @@
-caseDir=Jupp_Schultz 
-casePath=${PWD}/`dirname "$0"`
-# 1.
-cd $casePath
-rm ${caseDir}.zip
-zip -r ${caseDir}.zip $caseDir -x ${caseDir}/*00\* ${caseDir}/jupyter/data*\* 
+function zipCase()
+{
+    caseDir=$1 
+    casePath=${PWD}/`dirname "$0"`
+    # 1.
+    cd $casePath
+    rm ${caseDir}.zip
+    zip -r ${caseDir}.zip $caseDir -x ${caseDir}/*00\* ${caseDir}/jupyter/data*\* 
+}
+
+zipCase Jupp_Schultz
