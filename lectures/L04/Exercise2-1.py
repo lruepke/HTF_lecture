51c001d80f0422b77b937c481d4ca9190a8f7dc0
Travis Builder <lars.ruepke@gmail.com>
2020-12-01 17:53:33 +0000

    [ci skip] Autodoc commit for .

--- lectures/L04/Exercise2-1.py
+++ lectures/L04/Exercise2-1.py
@@ -1,11 +1,15 @@
 xmin,xmax=-1700,1700
 Tmin,Tmax=20,1000
-wGauss,x0=10,0
+wGauss,x0=400,0
+xigma = wGauss/2.355
+# see https://en.wikipedia.org/wiki/Full_width_at_half_maximum
 fig=plt.figure(figsize=(6,3))
 ax=plt.gca()
 x=np.linspace(xmin,xmax,500)
-y=Tmin+(Tmax-Tmin)*np.exp(-(x-x0)*(x-x0)/(2*wGauss*wGauss))
+y=Tmin+(Tmax-Tmin)*np.exp(-(x-x0)*(x-x0)/(2*xigma*xigma))
 ax.plot(x,y)
+ax.axvspan(x0-wGauss/2,x0+wGauss/2,ymax=0.5, alpha=0.5, color='lightgreen')
+ax.text(0.5,0.25,'FWHM\n%.0f m'%(wGauss), va='center',ha='center',transform=ax.transAxes)
 ax.set_ylabel('Temperature ($^{\circ}$C)')
 ax.set_xlabel('Distance (m)')
 ax.set_title('Gaussian shape temperature boundary condition')
