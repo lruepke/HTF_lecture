6578297fb05fc6bc68b6de4592c6d27abe060e8f
zguo <zguo@geomar.de>
2020-12-02 13:12:51 +0100

    add Driesner2010 cases

--- source/lectures/L04/cases/Driesner2010/HS1000/run_cases.sh
+++ source/lectures/L04/cases/Driesner2010/HS1000/run_cases.sh
@@ -3,7 +3,7 @@ function run_group()
     group=$1
     for k_df in 1E-16 3E-16 5E-16 8E-16 1E-15 2E-15 3E-15 4E-15 6E-15 8E-15 1E-14 3E-14 6E-14 8E-14 1E-13 2E-13 3E-13 4E-13 1E-12
     do 
-        caseDir=DF_${k_df}
+        caseDir=${k_df}
         python run_case.py $group/$caseDir
     done
 }
