e05ef3bd26aeaf50cf65d79ef5becb6d8ece4eb9
zguo <zguo@geomar.de>
2020-12-02 13:34:13 +0100

    add Driesner2010 cases

--- source/lectures/L04/cases/casePackage.sh
+++ source/lectures/L04/cases/casePackage.sh
@@ -7,6 +7,14 @@ function zipCase()
     rm ${caseDir}.zip
     zip -r ${caseDir}.zip $caseDir -x ${caseDir}/*00\* ${caseDir}/jupyter/data*\* ${caseDir}/ventT.* ${caseDir}/VTK\*
 }
-
+function zipCaseDriesner()
+{
+    caseDir=$1 
+    casePath=${PWD}/`dirname "$0"`
+    # 1.
+    cd $casePath
+    rm ${caseDir}.zip
+    zip -r ${caseDir}.zip $caseDir 
+}
 zipCase Jupp_Schultz
-zipCase Driesner2010
+zipCaseDriesner Driesner2010
