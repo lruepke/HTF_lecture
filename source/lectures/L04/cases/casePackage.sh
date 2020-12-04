87ede7d18ecb46517e68a2ef6263429fef10337c
zguo <zguo@geomar.de>
2020-12-04 20:31:21 +0100

    update

--- source/lectures/L04/cases/casePackage.sh
+++ source/lectures/L04/cases/casePackage.sh
@@ -7,14 +7,6 @@ function zipCase()
     rm ${caseDir}.zip
     zip -r ${caseDir}.zip $caseDir -x ${caseDir}/*00\* ${caseDir}/jupyter/data*\* ${caseDir}/ventT.* ${caseDir}/VTK\*
 }
-function zipCaseDriesner()
-{
-    caseDir=$1 
-    casePath=${PWD}/`dirname "$0"`
-    # 1.
-    cd $casePath
-    rm ${caseDir}.zip
-    zip -r ${caseDir}.zip $caseDir 
-}
+
 zipCase Jupp_Schultz
-zipCaseDriesner Driesner2010
+zipCase Driesner2010
