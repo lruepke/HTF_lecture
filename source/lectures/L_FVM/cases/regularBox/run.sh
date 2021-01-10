4f46b0e380fce8368582c8ad3274520a4f7c1094
zguo <zguo@geomar.de>
2021-01-11 00:33:32 +0100

    update

--- source/lectures/L_FVM/cases/regularBox/run.sh
+++ source/lectures/L_FVM/cases/regularBox/run.sh
@@ -7,6 +7,12 @@ cd ${0%/*} || exit 1    # Run from this directory
 application=`getApplication`
 
 ./clean.sh
+# option 1 regular box
 runApplication blockMesh
+# option 2 gmsh unstructured mesh
+# gmsh gmsh/mesh.geo -3 -o gmsh/mesh.msh -format msh22
+# gmshToFoam gmsh/mesh.msh
+# changeDictionary
+
 runApplication $application
 # foamToVTK -useTimeName
