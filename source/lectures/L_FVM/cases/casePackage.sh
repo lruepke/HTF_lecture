50335743e668d0a269bb7871c8866e9d976663e4
zguo <zguo@geomar.de>
2021-01-10 19:39:08 +0100

    L_FVM step 1 mesh

---
+++ source/lectures/L_FVM/cases/casePackage.sh
@@ -0,0 +1,12 @@
+function zipCase()
+{
+    caseDir=$1 
+    casePath=${PWD}/`dirname "$0"`
+    # 1.
+    cd $casePath
+    rm ${caseDir}.zip
+    zip -r ${caseDir}.zip $caseDir -x ${caseDir}/*00\* ${caseDir}/jupyter/data*\* ${caseDir}/ventT.* ${caseDir}/VTK\*
+}
+
+zipCase regularBox
+zipCase test_laplacianFoam
