7de2f27f373d105d5019e5fc24b9fcc67b004fd7
Lars Ruepke <lruepke@geomar.de>
2024-07-08 13:33:51 +0200

    stokes solver

--- Summer2024/source/lecture7/python/mechanical2d.py
+++ Summer2024/source/lecture7/python/mechanical2d.py
@@ -16,7 +16,7 @@ def mechanical2d(Mesh, Materials, Solver):
             - 'Bc_ind': Boundary condition indices.
             - 'Bc_val': Boundary condition values.
         Materials (dict): Material properties containing:
-            - 'Mu': Shear modulus.
+            - 'Mu': Shear viscosity.
             - 'Rho': Density.
             - 'G': Gravity vector.
         Solver (dict): Solver switches:
@@ -53,9 +53,6 @@ def mechanical2d(Mesh, Materials, Solver):
 
     PF = 1e3 * np.max(Mu)
 
-    C1 = 4/3
-    C2 = 2/3
-
     # PREPARE INTEGRATION POINTS & DERIVATIVES wrt LOCAL COORDINATES
     IP_X, IP_w = ip_triangle(nip)
     Shapes = shp_deriv_triangle(IP_X, nnodel)
@@ -67,7 +64,7 @@ def mechanical2d(Mesh, Materials, Solver):
     Q_all   = np.zeros((nel, nedof * np_edof))
     invM_all= np.zeros((nel,np_edof*np_edof)) 
     Rhs_all = np.zeros(sdof)
-    Vel     = np.zeros(sdof) #initial T, not strictly needed
+    Vel     = np.zeros(sdof) 
 
     # MAKE ELDOF
     EL2DOF = np.zeros((nel,nedof), dtype=np.int32)
@@ -84,7 +81,7 @@ def mechanical2d(Mesh, Materials, Solver):
 
         # INTEGRATION LOOP
         A_elem = np.zeros((nedof, nedof))
-        Q_elem = np.zeros((nedof, np_edof))
+        Q_elem = np.zeros((np_edof,nedof))
         M_elem = np.zeros((np_edof, np_edof))
         Rhs_elem = np.zeros(nedof)
 
@@ -92,7 +89,7 @@ def mechanical2d(Mesh, Materials, Solver):
         P = np.ones((np_edof, np_edof))
         Pb = np.ones(np_edof)
 
-        P[:, 1:3] = ECOORD_X[:3]
+        P[1:3, :] = ECOORD_X[:3].T
         for ip in range(nip):
             # LOAD SHAPE FUNCTIONS DERIVATIVES FOR INTEGRATION POINT
             Ni      = Shapes['shape_functions'][ip]
@@ -101,13 +98,20 @@ def mechanical2d(Mesh, Materials, Solver):
             Pi      = np.linalg.solve(P, Pb)
 
             # CALCULATE JACOBIAN, ITS DETERMINANT AND INVERSE
-            Jac       = dNdui @ ECOORD_X
+            #Jac       = (dNdui @ ECOORD_X).T
+            Jac        = dNdui @ ECOORD_X
 
             detJ    = np.linalg.det(Jac)
             invJ    = np.linalg.inv(Jac)
 
             # DERIVATIVES wrt GLOBAL COORDINATES
-            dNdX    = invJ @ dNdui 
+            #dNdX    = (dNdui.T @ invJ).T
+            dNdX    = invJ @ dNdui
+
+            #check if dNdX1 and dNdX are the same
+            #if np.absolute(dNdX - dNdX1).max()>1e-10:
+            #    raise ValueError("Jacobian")
+
 
             # NUMERICAL INTEGRATION OF ELEMENT MATRICES
             weight = IP_w[ip] * detJ
@@ -115,16 +119,16 @@ def mechanical2d(Mesh, Materials, Solver):
             B[1,1::2] = dNdX[1]
             B[2,0::2] = dNdX[1]
             B[2,1::2] = dNdX[0]
-            m         = np.array([1,1,0])
+            Bvol = dNdX.T
 
             A_elem += weight * EMu * (B.T @ DEV @ B)
-            Q_elem -= weight * B.T @ m[:, np.newaxis] @ Pi[np.newaxis, :]
+            Q_elem -= weight*Pi[:,np.newaxis] @ Bvol.ravel()[np.newaxis,:]
             M_elem += weight * Pi[:, np.newaxis] @ Pi[np.newaxis, :]
             Rhs_elem += weight * ERho * (G[:, np.newaxis] @ Ni[np.newaxis, :]).T.ravel()
 
         # STATIC CONDENSATION
         invM_elem = np.linalg.inv(M_elem)
-        A_elem += PF * (Q_elem @ invM_elem @ Q_elem.T)
+        A_elem += PF * (Q_elem.T @ invM_elem @ Q_elem)
 
         # WRITE DATA INTO GLOBAL STORAGE
         A_all[iel, :] = A_elem.ravel() #A_elem[np.triu_indices(nedof)]
@@ -133,13 +137,12 @@ def mechanical2d(Mesh, Materials, Solver):
         Rhs_all[EL2DOF[iel,:]] += Rhs_elem
 
         # assemble coefficients
-        I[iel,:]  =  (EL2DOF[iel,:]*np.ones((nedof,1), dtype=int)).T.reshape(nedof*nedof)
-        J[iel,:]  =  (EL2DOF[iel,:]*np.ones((nedof,1), dtype=int)).reshape(nedof*nedof)
+        I[iel, :] = np.tile(EL2DOF[iel, :], (nedof, 1)).T.ravel()
+        J[iel, :] = np.tile(EL2DOF[iel, :], (nedof, 1)).ravel()
 
 
     # ASSEMBLE GLOBAL MATRICES
-    A_all = csr_matrix((A_all.reshape(nel*nedof*nedof),(I.reshape(nel*nedof*nedof),J.reshape(nel*nedof*nedof))),shape=(sdof,sdof))
-
+    A_all = csr_matrix((A_all.ravel(),(I.ravel(),J.ravel())),shape=(sdof,sdof))
 
     # smart way of boundary conditions that keeps matrix symmetry
     Free    = np.arange(0,sdof)
@@ -151,7 +154,27 @@ def mechanical2d(Mesh, Materials, Solver):
     Vel[Free] = spsolve(A_all[np.ix_(Free, Free)],Rhs_all[Free])
     Vel[Bc_ind] = Bc_val
 
-    # compute pressure
-    
+    # compute big matrices for pressure update
+    # logic is that each element as three pressure dofs, as the pressure is dicsontinuous, we make ad-hoc numbering. 
+    # element[0] -> pressure = [0,1,2]
+    # element[1] -> pressure = [3,4,5]
+    # The velocity dofs come from EL2DOF
 
-    return Vel
+    Q_i = np.tile(np.arange(0, nel*np_edof, dtype=np.int32), (nedof,1)).T
+    Q_j = np.tile(EL2DOF, (1,np_edof))
+    Q_all = csr_matrix((Q_all.ravel(), (Q_i.ravel(), Q_j.ravel())), shape=(nel*np_edof, sdof))
+
+    invM_i = np.tile(np.arange(0, nel*np_edof, dtype=np.int32), (np_edof, 1)).T
+    #invM_j = np.tile(np.arange(np_edof, dtype=np.int32), np_edof)...
+    # Create the base sequence for a single element's DOFs
+    base_sequence = np.tile(np.arange(np_edof), nel * np_edof)
+    # Offset each block for the number of elements
+    offsets = np.repeat(np.arange(nel) * np_edof, np_edof**2)
+    # Create the column indices by adding the base sequence and offsets
+    column_indices = base_sequence + offsets
+    invM_all = csr_matrix((invM_all.ravel(), (invM_i.ravel(), column_indices.ravel())), shape=(nel*np_edof, nel*np_edof))    
+    
+    #Pressure = (PF*invM_all.dot(Q_all.dot(Vel)))
+    Pressure = (PF*invM_all @ (Q_all @ Vel))
+
+    return Vel, Pressure
