595c810d23472f9d5921a1d353bb5f4b46ee8751
lruepke <lruepke@users.noreply.github.com>
2026-01-07 17:52:11 +0000

    deploy: 327a3e7a9da7c55b1966a8c366d0569ddf2c2b11

--- winter2025/_downloads/b90ebeeefa0632f33f1fca2dfa23d8e5/shapes_tri.py
+++ winter2025/_downloads/b90ebeeefa0632f33f1fca2dfa23d8e5/shapes_tri.py
@@ -1,30 +1,47 @@
 import numpy as np
 
-def shapes_tri(xi, eta):
-    #shape functions
+
+def shapes_tri(xi: float, eta: float) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Compute shape functions and their derivatives for triangular elements.
+
+    Parameters
+    ----------
+    xi : float
+        First local coordinate (0 to 1)
+    eta : float
+        Second local coordinate (0 to 1)
+
+    Returns
+    -------
+    N : np.ndarray of shape (3,)
+        Shape function values at integration point
+    dNds : np.ndarray of shape (2, 3)
+        Derivatives of shape functions with respect to local coordinates
+    """
+    # shape functions
     eta2 = xi
     eta3 = eta
-    eta1 = 1-eta2-eta3
-
+    eta1 = 1 - eta2 - eta3
 
     N1 = eta1
     N2 = eta2
     N3 = eta3
 
     N = np.array([N1, N2, N3])
-    
+
     # and their derivatives
-    dNds = np.zeros((2,3))
+    dNds = np.zeros((2, 3))
 
-    dNds[0,0]   =  -1 #derivative 
-    dNds[1,0]   =  -1 #derivative 
+    dNds[0, 0] = -1  # derivative
+    dNds[1, 0] = -1  # derivative
 
-    #derivatives of second shape function with local coordinates
-    dNds[0,1]   =  1
-    dNds[1,1]   =  0
+    # derivatives of second shape function with local coordinates
+    dNds[0, 1] = 1
+    dNds[1, 1] = 0
 
-    #derivatives of third shape function with local coordinates
-    dNds[0,2]   =  0
-    dNds[1,2]   =  1
+    # derivatives of third shape function with local coordinates
+    dNds[0, 2] = 0
+    dNds[1, 2] = 1
 
     return N, dNds
